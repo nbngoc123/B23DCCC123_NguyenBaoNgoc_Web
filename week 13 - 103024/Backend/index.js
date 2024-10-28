0ed188b368e5d4816beeb99bb6b6ba33e9feefa7
nbngoc123 <nbngoc.udu.ptit@gmail.com>
2024-10-28 16:01:24 +0700

    update api

--- week 13 - 103024/Backend/index.js
+++ week 13 - 103024/Backend/index.js
@@ -5,40 +5,63 @@ const cors = require('cors');
 const app = express();
 const port = 3001;
 
-// Bật CORS để cho phép frontend truy cập
 app.use(cors());
-app.use(express.urlencoded({ extended: true })); // Để xử lý dữ liệu POST URL-encoded
-app.use(express.json()); // Để xử lý dữ liệu POST JSON
-
-// Định nghĩa endpoint POST tại /api/generate
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.post('/api/generate', async (req, res) => {
-  const prompt = req.body.prompt; // Lấy prompt từ dữ liệu POST
+  const prompt = req.body.prompt;
 
   try {
-    // Khởi tạo GoogleGenerativeAI với API_KEY
     const genAI = new GoogleGenerativeAI(process.env.API_KEY);
     const model = genAI.getGenerativeModel({ 
-      model: "tunedModels/todolistdataupdatekey2-v49pqdz54ztj",
+      model: "tunedModels/todolistdataupdatekey2-v49pqdz54ztj",  // con này chỉ thu thập data từ note users
     });
 
-    // Gửi prompt đến model AI và nhận phản hồi
     const result = await model.generateContent(prompt);
     const responseText = result.response.text();
 
-    // Log phản hồi để debug
     console.log("Response text:", responseText);
 
-    // Trả phản hồi cho client
     res.json({ response: responseText });
 
   } catch (error) {
-    // Xử lý lỗi khi gọi GoogleGenerativeAI
     console.error("Lỗi khi gọi AI:", error);
     res.status(500).json({ error: 'Có lỗi xảy ra khi gọi Google Generative AI.' });
   }
 });
 
-// Khởi động server và lắng nghe yêu cầu
+app.post('/chats/withnote', async (req, res) => {
+  const prompt = req.body.prompt;
+
+  try {
+    const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+    const model = genAI.getGenerativeModel({ 
+      model: "tunedModels/todolistdata3-4t8eoouosa67",  
+    });
+
+    // con này chuyên để chat với data của người dùng
+    // Model ID: tunedModels/todolistdata3-4t8eoouosa67
+    // Base model: Gemini 1.5 Flash 001 Tuning
+    //  Total training time: 6h 28m
+    //  Tuned examples: 197 examples 
+    // Epochs: 45
+    //  Batch size: 16 
+    //  Learning rate: 0.001 
+
+
+    const result = await model.generateContent(prompt);
+    const responseText = result.response.text();
+
+    console.log("Response text:", responseText);
+
+    res.json({ response: responseText });
+
+  } catch (error) {
+    console.error("Lỗi khi gọi AI", error);
+    res.status(501).json({ error: ' quá nhiều requests' });
+  }
+})
+
 app.listen(port, () => {
   console.log(`Server đang chạy tại http://localhost:${port}/`);
 });
